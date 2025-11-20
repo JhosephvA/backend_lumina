3f3cd18dbf152bb8a9646a15319cb433738f1c6d
JhosephvA <henry.ventura@tecsup.edu.pe>
2025-11-20 10:01:18 -0500

    otro new

--- src/config/sequelize-config.js
+++ src/config/sequelize-config.js
@@ -2,17 +2,17 @@
 const { Sequelize } = require('sequelize');
 require('dotenv').config();
 
-// Usar la URL pública de Railway
-const connectionUrl = process.env.MYSQL_PUBLIC_URL;
+// Usar la URL interna de Railway para conexiones dentro de la misma red
+const connectionUrl = process.env.DB_URL;
 
 const sequelize = new Sequelize(connectionUrl, {
   dialectOptions: {
     ssl: {
       require: true,
-      rejectUnauthorized: false, // importante para conexiones remotas
+      rejectUnauthorized: false, // importante para conexiones remotas seguras
     },
   },
-  logging: false, // desactiva logs de SQL
+  logging: false, // desactiva logs SQL
 });
 
 module.exports = sequelize;
