a9651a19b3850467717d578e43052ec0948e076d
JhosephvA <henry.ventura@tecsup.edu.pe>
2025-11-20 09:41:33 -0500

    new

--- src/config/sequelize-config.js
+++ src/config/sequelize-config.js
@@ -1,24 +1,18 @@
 // sequelize-config.js
 const { Sequelize } = require('sequelize');
+require('dotenv').config();
 
-const dbPort = process.env.DB_PORT ? parseInt(process.env.DB_PORT) : 3306;
+// Usar la URL pública de Railway
+const connectionUrl = process.env.MYSQL_PUBLIC_URL;
 
-const sequelize = new Sequelize(
-  process.env.DB_NAME,     // "railway"
-  process.env.DB_USER,     // "root"
-  process.env.DB_PASSWORD, // tu password
-  {
-    host: process.env.DB_HOST, // "mainline.proxy.rlwy.net"
-    port: dbPort,              // 13165
-    dialect: process.env.DB_DIALECT || 'mysql',
-    logging: false,
-    dialectOptions: {
-      ssl: {
-        require: true,
-        rejectUnauthorized: false
-      }
-    }
-  }
-);
+const sequelize = new Sequelize(connectionUrl, {
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false, // importante para conexiones remotas
+    },
+  },
+  logging: false, // desactiva logs de SQL
+});
 
 module.exports = sequelize;
