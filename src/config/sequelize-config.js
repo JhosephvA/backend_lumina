baa8aeacab7b7cc49a659d4ae2a0fd51b86b40d6
JhosephvA <henry.ventura@tecsup.edu.pe>
2025-11-20 09:29:21 -0500

    otra vez

--- src/config/sequelize-config.js
+++ src/config/sequelize-config.js
@@ -1,22 +1,7 @@
-// sequelize-config.js
 const { Sequelize } = require('sequelize');
 
-// Configuración básica para MySQL
-const dbConfig = {
-  dialect: process.env.DB_DIALECT || 'mysql',
-  logging: false,
-  dialectOptions: {
-    ssl: {
-      require: true,
-      rejectUnauthorized: false, // obligatorio en Railway DB
-    },
-  },
-};
-
-// Asegurarse de que el puerto sea número
 const dbPort = process.env.DB_PORT ? parseInt(process.env.DB_PORT) : 3306;
 
-// Conexión
 const sequelize = new Sequelize(
   process.env.DB_NAME,
   process.env.DB_USER,
@@ -24,7 +9,14 @@ const sequelize = new Sequelize(
   {
     host: process.env.DB_HOST,
     port: dbPort,
-    ...dbConfig,
+    dialect: process.env.DB_DIALECT || 'mysql',
+    logging: false,
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false
+      }
+    }
   }
 );
 
