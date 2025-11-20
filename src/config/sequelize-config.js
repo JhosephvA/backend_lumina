705e34d4cbc44c1626f4805e1faafb8815d875e8
JhosephvA <henry.ventura@tecsup.edu.pe>
2025-11-20 09:31:42 -0500

    bag

--- src/config/sequelize-config.js
+++ src/config/sequelize-config.js
@@ -1,14 +1,15 @@
+// sequelize-config.js
 const { Sequelize } = require('sequelize');
 
 const dbPort = process.env.DB_PORT ? parseInt(process.env.DB_PORT) : 3306;
 
 const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASSWORD,
+  process.env.DB_NAME,     // "railway"
+  process.env.DB_USER,     // "root"
+  process.env.DB_PASSWORD, // tu password
   {
-    host: process.env.DB_HOST,
-    port: dbPort,
+    host: process.env.DB_HOST, // "mainline.proxy.rlwy.net"
+    port: dbPort,              // 13165
     dialect: process.env.DB_DIALECT || 'mysql',
     logging: false,
     dialectOptions: {
